f4a3e6cbc8a96027c6e9eb6c74095e1163763a92
vega <devid367552@gmail.com>
2022-11-13 20:11:44 +0300

    v0.06

--- src/vsu/cs/vega/Main.java
+++ src/vsu/cs/vega/Main.java
@@ -1,15 +1,21 @@
 package vsu.cs.vega;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Solution output = new Solution();
+    public static void main(String[] args)  {
         String error = "You entered a non-rectangular matrix, please check the data and re-enter.";
-        try {
+        try{
+        Solution output = new Solution();
+
+
             output.readMtx();
         }
         catch (ArrayIndexOutOfBoundsException exception){
             System.err.print(error);
+        }
+        catch (IOException ex){
+            System.out.println("not work");
         }
 
 
